77317bcb3c26322d8586e31a4c9488d9fe9a1eb6
Fahri Akbaba <fahriakbaba@gmail.com>
2022-12-13 21:26:28 +0300

    Home component is created and styled

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const database = [
     {
         "id": 1,
-        "name": "D Germann",
+        "author": "D Germann",
         "title": "[Physiotherapy in lumbar disc herniation]",
         "body": "Physiotherapy is the treatment of choice in patients with symptoms caused by a lumbar disc herniation. In clinical practice a broad range of physiotherapeutic modalities has been revealed to be helpful. During the acute stage the efficacy of the McKenzie-concept, mobilisation therapies and traction has been demonstrated in randomized controlled trials with a blind assessor. In addition, pain reducing physical therapies such as cold or electrotherapy and non-steroidal anti-inflammatory drugs, analgesics and/or muscle relaxants are sensible initial accompanying treatments. The effectiveness of active physiotherapies such as training of local strength endurance of back and abdominal muscles has been proven in patients during the chronic stage. The indications for a in-patient rehabilitation programme, for surgery and the danger of developing chronic low back pain are discussed.",
         "views": 99,
@@ -11,7 +11,7 @@ const database = [
     },
     {
         "id": 2,
-        "name": "Tamás Bender",
+        "author": "Tamás Bender",
         "title": "[Evidence-based physiotherapy]",
         "body": "This article on physiotherapy presents some current evidence stating the strengths and weaknesses of the physiotherapeutic procedures. In the area of physiotherapy empirical data obtained during decades were overtaken by evidence from current studies. The author points out the great problem of physiotherapy, namely the heterogeneity of the applied parameters. Knowledge of current evidence may be very important and helpful for the physicians, but the author proposes, from the practical point of view, that physiotherapeutical procedures based on exprience and used for many years should not be entirely neglected. Nowadays physiotherapy plays an important role in the treament of locomotor diseases but its use is increasing in other fields of medicine, as well.",
         "views": 87,
@@ -20,7 +20,7 @@ const database = [
     },
     {
         "id": 3,
-        "name": "Ming Yang",
+        "author": "Ming Yang",
         "title": "Chest physiotherapy for pneumonia in adults",
         "body": "Despite conflicting evidence, chest physiotherapy has been widely used as an adjunctive treatment for adults with pneumonia. To assess the effectiveness and safety of chest physiotherapy for pneumonia in adults. We searched CENTRAL 2012, Issue 11, MEDLINE (1966 to November week 2, 2012), EMBASE (1974 to November 2012), Physiotherapy Evidence Database (PEDro) (1929 to November 2012), CINAHL (2009 to November 2012) and CBM (1978 to November 2012). Randomised controlled trials (RCTs) assessing the efficacy of chest physiotherapy for treating pneumonia in adults. Two authors independently assessed trial eligibility, extracted data and appraised trial quality. Primary outcomes were mortality and cure rate. We used risk ratios (RR) and mean difference (MD) for individual trial results in the data analysis. We performed meta-analysis and measured all outcomes with 95% confidence intervals (CI). Based on current limited evidence, chest physiotherapy might not be recommended as routine additional treatment for pneumonia in adults.",
         "views": 34,
@@ -29,7 +29,7 @@ const database = [
     },
     {
         "id": 4,
-        "name": "Laura Landesa-Piñeiro",
+        "author": "Laura Landesa-Piñeiro",
         "title": "Physiotherapy treatment of lateral epicondylitis: A systematic review",
         "body": "Lateral epicondylitis is a tendinopathy with a prevalence of between 1-3% of the population aged 35-54 years. It is a pathology with a favorable evolution, but with frequent recurrences (which imply an economic extra cost).The objective of this review was to determine the efficacy of physiotherapy treatment for the treatment of epicondylitis and, if any, to identify the most appropriate techniques. A systematic search was carried out in October 2020 in the databases of PubMed, Cinahl, Scopus, Medline and Web of Science using the search terms: Physical therapy modalities, Physical and rehabilitation medicine, Rehabilitation, Tennis elbow and Elbow tendinopathy. Nineteen articles were found, of which seven applied shock waves, three applied orthoses, three applied different manual therapy techniques, two applied some kind of bandage, one applied therapeutic exercise, one applied diacutaneous fibrolysis, one applied high intensity laser, and one applied vibration. Manual therapy and eccentric strength training are the two physiotherapeutic treatment methods that have the greatest beneficial effects, and, furthermore, their cost-benefit ratio is very favorable. Its complementation with other techniques, such as shock waves, bandages or Kinesio® taping, among others, facilitates the achievement of therapeutic objectives, but entails an added cost.",
         "views": 55,
@@ -38,7 +38,7 @@ const database = [
     },
     {
         "id": 5,
-        "name": "Nora Shields",
+        "author": "Nora Shields",
         "title": "Physiotherapy management of Down syndrome",
         "body": "Over the past century, the life expectancy of people born with Down syndrome has increased from 9 to 60 years,1 with data indicating that it could match the general population within a generation.2 This rapid increase in survival is largely due to earlier surgical correction of heart defects, better treatment of infections and improved general healthcare.3 This success, however, is tempered by the substantially higher risk and early onset of several chronic health conditions in adults with Down syndrome, particularly cognitive decline commencing in their early 40s. ",
         "views": 27,
@@ -47,7 +47,7 @@ const database = [
     },
     {
         "id": 6,
-        "name": "Catherine L Granger ",
+        "author": "Catherine L Granger ",
         "title": "Physiotherapy management of lung cancer",
         "body": "Lung cancer is associated with significant morbidity and mortality and is a substantial burden to healthcare systems. Physiotherapists play an important role in the management of people with lung cancer. Advances in research over the past decade, particularly supporting the use of exercise training, have rapidly progressed the role of physiotherapy in lung cancer. This review summarises the burden associated with lung cancer, the management of lung cancer with a particular focus on physiotherapy interventions, and future directions for research and clinical practice.",
         "views": 51,
@@ -56,7 +56,7 @@ const database = [
     },
     {
         "id": 7,
-        "name": "Sevgi Ozalevli",
+        "author": "Sevgi Ozalevli",
         "title": "Impact of physiotherapy on patients with advanced lung cancer",
         "body": "Patients with lung cancer have high mortality and high morbidity. Lung cancer-related symptoms and problems such as dyspnea, fatigue, pain, and cachexia that begin in the early phase later result in poor physical functioning, psychosocial, and quality of life status. In addition, advancing age is associated with significant comorbidity. These patients may benefit from multidisciplinary therapy to reduce the perceived severity of dyspnea and fatigue and increase physical functioning and quality of life. Based on management of symptoms and problems such as dyspnea, physical inactivity, cancer-related fatigue, respiratory secretions, pain, and anxiety-depression of these patients, it is thought that physiotherapy techniques can be used on advanced lung cancer patients following a comprehensive evaluation. However, well-designed, prospective, and randomized-controlled trials are needed to prove the efficacy of physiotherapy and pulmonary rehabilitation in general for patients with advanced lung cancer.",
         "views": 251,
@@ -65,7 +65,7 @@ const database = [
     },
     {
         "id": 8,
-        "name": "D S Selsby",
+        "author": "D S Selsby",
         "title": "Chest physiotherapy",
         "body": "Respiratory physiotherapy is a core specialty within the physiotherapy profession and occupies a key role in the management and treatment of patients with respiratory diseases. It aims to unclog the patient’s airways and help them return to physical activity and exertion. The respiratory physiotherapist employs many diverse interventions, including pulmonary rehabilitation, early mobilisation, and airway clearance techniques, all having beneficial effects on the symptoms associated with respiratory diseases. For example improved sputum clearance and cough efficacy, reduced dyspnea, and improved physical fitness. The beneficial effects are demonstrated in improved functional ability and reduced intensive care and hospital stay, with savings in associated health-care costs. Physiotherapists specialising in respiratory care work in a variety of settings including Intensive Care Units (ICUs), hospital wards, and primary care settings. The purpose of chest physiotherapy are: To facilitate removal of retained or profuse airway secretions. To optimize lung compliance and the ventilation- perfusion ratio / improve gas exchange. To decrease the work of breathing. Improve exercise tolerance Prevent secondary complications.",
         "views": 729,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home({data}) {
   console.log("home page => data: ", data);
   return (
-    <div>Home</div>
+    <section className="home">
+      {
+        data.map(item => (
+          <article key={item.id}>
+            <h3>{item.title}</h3>
+            <Link to={`/profile/${item.id}`} className="author">{item.author}</Link>
+            <p>{item.body.substring(0,100)}</p>
+            <button>
+              <Link to={`/article/${item.id}`} className="home-btn">More Details</Link>
+            </button>
+          </article>
+        ))
+      }
+    </section>
   )
 }
 
