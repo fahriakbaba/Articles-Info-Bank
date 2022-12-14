e38ef51546575f1e63a1eebc6288cdda489ae9b7
Fahri Akbaba <fahriakbaba@gmail.com>
2022-12-14 20:17:20 +0300

    ImageSlider component is created and styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import ImageSlider from '../components/ImageSlider';
 
-function Home({data}) {
-   
+function Home({ data }) {
+  const containerStyles = {
+    width: "650px",
+    height: "280px",
+    margin: "1.5rem auto 7rem auto",
+  };
+
   return (
     <section className="home">
+      <div style={containerStyles}>
+        <ImageSlider slides={data.filter(item => item.views >= 90)} />
+      </div>
       {
         data.map(item => (
           <article key={item.id}>
             <h3>{item.title}</h3>
             <Link to={`profile/${item.id}`} className="author">{item.author}</Link>
-            <p>{item.body.substring(0,150)}...</p>
+            <p>{item.body.substring(0, 150)}...</p>
             <button>
               <Link to={`article/${item.id}`} className="home-btn">More Details</Link>
             </button>
